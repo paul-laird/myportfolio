c97520fc4be6450ad0a9e6bc25f02c63842b0674
paul-laird <35932444+paul-laird@users.noreply.github.com>
2020-03-24 13:32:34 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ class test:
         self.greeting=greeting
     def greet(self,name):
         print(self.greeting,name)
-    def ignore(self):
-        print('')
+    def ignore(self,X='q'):
+        print('Not doing anything with X')
